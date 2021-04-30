d29e5926154c50cb7859ed1287bf4065d1f8acc7
Advaith Hegde <77739878+Eagleye007@users.noreply.github.com>
2021-04-30 21:29:41 +0530

    Delete file extension.py

--- file extension.py
+++
@@ -1,8 +0,0 @@
-#Program to input file extensions
-fileName=input("Write the file name")
-file_extns=filename.split(".")
-print("The extension is" +repr(file_extns[-1]))
-
-#OUTPUT:
-#Write the file name:abc.py
-#The extension is 'py'
